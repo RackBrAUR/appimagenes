49da60d53ba18498bd1db69ba8f8d628a6445903
RackBrAUR <felipe.rua.verdolaga77@gmail.com>
2022-12-03 11:04:10 -0500

    subida2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Buscador from './componentes/Buscador';
+import Resultado from './componentes/Resultado';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state= {
+    termino : 'cafe',
+    imagenes : []
+  }
+
+  consultarApi = () => {
+    const termino = this.state.termino;
+    const url = 'https://pixabay.com/api/?key=31801074-fc59285cfbb78ce3562568f73&q=$'+'{'+ termino + '}';
+
+    //console.log(url);
+    fetch(url)
+    .then(respuesta => respuesta.json()) 
+    .then(resultado => this.setState({ imagenes : resultado.hits }))
+  }
+
+  datosBusqueda = (termino)=> {
+    this.setState({
+      termino
+    }, ()=> {
+      this.consultarApi();
+    })
+  }
+  
+  render() {
+    return (
+     <div className="app container">
+       <div className="jumbotron" >
+          <p className="lead text-center">Buscador de imagenes</p>
+
+          <Buscador 
+            datosBusqueda={this.datosBusqueda}
+          />
+       </div>
+       <Resultado 
+        imagenes = {this.state.imagenes}
+       />
+     </div>
+   );
+  }
 }
 
 export default App;
